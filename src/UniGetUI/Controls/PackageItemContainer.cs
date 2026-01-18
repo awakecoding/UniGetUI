6e132b9cfccbc35b1738c129aff98896a8e9ed63
Marc-André Moreau <mamoreau@devolutions.net>
2026-01-18 11:55:51 -0500

    Fix XAML errors: PackageManager Description to Content, AbstractPackagesPage property fixes, PackageItemContainer StyledProperties (78->56 errors)

--- src/UniGetUI/Controls/PackageItemContainer.cs
+++ src/UniGetUI/Controls/PackageItemContainer.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using UniGetUI.PackageEngine.Interfaces;
 using UniGetUI.PackageEngine.PackageClasses;
 
@@ -6,7 +7,22 @@ namespace UniGetUI.Interface.Widgets
 {
     public partial class PackageItemContainer : ContentControl
     {
-        public IPackage? Package { get; set; }
-        public PackageWrapper Wrapper { get; set; } = null!;
+        public static readonly StyledProperty<IPackage?> PackageProperty =
+            AvaloniaProperty.Register<PackageItemContainer, IPackage?>(nameof(Package));
+
+        public IPackage? Package
+        {
+            get => GetValue(PackageProperty);
+            set => SetValue(PackageProperty, value);
+        }
+
+        public static readonly StyledProperty<PackageWrapper> WrapperProperty =
+            AvaloniaProperty.Register<PackageItemContainer, PackageWrapper>(nameof(Wrapper));
+
+        public PackageWrapper Wrapper
+        {
+            get => GetValue(WrapperProperty);
+            set => SetValue(WrapperProperty, value);
+        }
     }
 }
